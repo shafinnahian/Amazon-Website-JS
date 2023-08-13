13d5d8e2434aae1dae430ab7b8423519d43d2d43
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-08-13 20:18:32 +0600

    add selector functionality

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-            <select>
+            <select class = "js-product-quantity-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -61,6 +61,8 @@ document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () => {
             const productId = button.dataset.productId;
+            const quantityMulti = document.querySelector(`.js-product-quantity-${productId}`)
+                .value;
             
             let matchingItem = '';
 
@@ -71,11 +73,11 @@ document.querySelectorAll('.js-add-to-cart')
             })
 
             if (matchingItem){
-                matchingItem.quantity += 1;
+                matchingItem.quantity += Number(quantityMulti);
             } else {
                 cart.push({
                     productId: productId,
-                    quantity: 1
+                    quantity: Number(quantityMulti)
                 });                
             }
 
